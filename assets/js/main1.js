64ba5d7bcd8405737700c0a6f5d68103f21e3e7f
MrGaggio <steruggiu@gmail.com>
2021-11-30 09:55:34 +0000

    creata struttura

---
+++ assets/js/main1.js
@@ -0,0 +1,15 @@
+/* 
+jsnack 1
+Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
+*/
+
+
+// Lista di nomi
+const nomi = ["Stefano", "Marco", "Luca", "Giovanni", "Amedeo", "Silvia", "Ilaria"]
+
+// Lista di cognomi
+const cognomi = ["Rossi", "Russo", "Ferrari", "Esposito", "Bianchi", "Romano", "Colombo"]
+
+// Bisogna generare una falsa lista di invitati 
+const falsaLista = []
+
